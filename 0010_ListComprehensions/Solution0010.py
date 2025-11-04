8fc982346a339395ace9864e4d5fce65aef00d69
{aknikn} <{akane.ikeno@gmail.com}>
2025-11-04 21:57:32 +0900

    Solved 0010 to 0011

--- 0010_ListComprehensions/Solution0010.py
+++ 0010_ListComprehensions/Solution0010.py
@@ -1,2 +1,14 @@
-greeding = "Hello, World!"
-print(greeding)
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+# ijkArray: All permutations of [i, j, k]
+ijkArray = []
+for i in range(0, x+1):
+    for j in range(0, y+1):
+        for k in range(0, z+1):
+            if i+j+k!=n:
+                ijkArray.append([i, j, k])
+
+print(ijkArray)
