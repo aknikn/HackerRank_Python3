2c8a780bbeed219d818b14886b8ce0906a894d6b
aknikn <147521640+aknikn@users.noreply.github.com>
2026-01-26 21:54:39 +0900

    Solved 0025

--- 0025_Alphabet_Rangoli/Solution0025.py
+++ 0025_Alphabet_Rangoli/Solution0025.py
@@ -1,2 +1,16 @@
-greeding = "Hello, World!"
-print(greeding)
+def print_rangoli(size):
+    # my code goes below
+    data = "abcdefghijklmnopqrstuvwxyz"
+    alpha = [data[i] for i in range(size)]
+    items = list(range(size))
+    items = items[:-1]+items[::-1]
+    
+    for i in items:
+        temp = alpha[-(i+1):]
+        row = temp[::-1]+temp[1:]
+        print("-".join(row).center(n*4-3, "-"))
+
+# my code goes above
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
